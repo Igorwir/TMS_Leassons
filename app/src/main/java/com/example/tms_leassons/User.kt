6bf2b96f279b4f4d862750b74e312e685f54d586
igor vyrvich <igorwir@mail.ru>
2022-04-21 22:19:31 +0300

    add Task 2

--- app/src/main/java/com/example/tms_leassons/User.kt
+++ app/src/main/java/com/example/tms_leassons/User.kt
@@ -1,22 +1,26 @@
 package com.example.tms_leassons
 
 
-
-
-class User(name:String,secondName:String,age:Int) {
-
+ class User(val name: String, val secondName: String, val age: Int) {
 }
 
 
-var listUsers:MutableList<User> = mutableListOf()
+var listUsers = mutableListOf<User>()
+
+val user = User("Bob", "Smith", 24)
+val user2 = User("Jon", "Bond", 30)
 fun main() {
- val user = User("Bob", "Smith", 24)
- val user2 = User("Jon", "Bond", 30)
- addUser(user)
-
- listUsers.forEach{ println(it.toString())}
+    addUser(user)
+    addUser(user2)
+    showList()
 }
- fun addUser(us: User) {
-  listUsers.add(us)
 
- }
+fun addUser(us: User) {
+    listUsers.add(us)
+
+}
+fun showList() {
+    listUsers.forEach {
+        println("Name: ${it.name}  ${it.secondName} age: ${it.age}")
+    }
+}
