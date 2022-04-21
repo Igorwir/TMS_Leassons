08e2bba6567ea252cf5a3a2a1df2350194ce68e4
igor vyrvich <igorwir@mail.ru>
2022-04-21 11:20:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tms_leassons/Randoms.kt
@@ -0,0 +1,37 @@
+package com.example.tms_leassons
+
+import android.util.Log
+import java.util.*
+
+
+class Randoms {
+
+
+}
+
+fun main() {
+    val v1 = "Да"
+    val v2 = "Нет"
+    val v3 = "Скорее всего да"
+    val v4 = "Скорее всего нет"
+    val v5 = "Возможно"
+    val v6 = "Имеются перспективы"
+    val v7 = "Вопрос задан неверно"
+    val random = arrayOf(v1, v2, v3, v4, v5, v6, v7)
+    val arraySize = random.size
+    val rand = Random().nextInt(arraySize)
+    println("Input your question :")
+    val line = readLine()
+    if (line != null) {
+        val resolt = random[rand]
+        println(resolt)
+
+    }
+}
+
+
+
+
+
+
+
