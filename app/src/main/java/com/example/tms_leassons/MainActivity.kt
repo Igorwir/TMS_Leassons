df9aa750c09270e77506084e94c4af5036fe2fea
igor vyrvich <igorwir@mail.ru>
2022-04-23 14:36:25 +0300

    Added task 2 from dz 15

--- app/src/main/java/com/example/tms_leassons/MainActivity.kt
+++ app/src/main/java/com/example/tms_leassons/MainActivity.kt
@@ -1,23 +1,37 @@
 package com.example.tms_leassons
 
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-
+import android.view.View
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
 
+    val rnds = (5..155).random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-
+        val rnds = (5..155).random()
     }
+    @SuppressLint("SetTextI18n")
+    fun onclick(view: View) {
+        val tv = findViewById<TextView>(R.id.tx)
+        if (rnds in (25..100)) {
+            tv.text = "Range number $rnds"
 
+        } else {
+            tv.text = " No Range number $rnds"
+
+        }
+    }
 }
+
+
+
+
 
