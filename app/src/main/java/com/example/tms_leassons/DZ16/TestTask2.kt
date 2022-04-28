d07047d685bf43b5fa39c6fdd32ece1c0fc25bef
igor vyrvich <igorwir@mail.ru>
2022-04-28 17:27:34 +0300

    Delete  sam lines

--- app/src/main/java/com/example/tms_leassons/DZ16/TestTask2.kt
+++ app/src/main/java/com/example/tms_leassons/DZ16/TestTask2.kt
@@ -7,8 +7,7 @@ import java.lang.Integer.max
 //Создать функциональную переменную для выражение макс(а*б*с, а+б+с)
 @RequiresApi(Build.VERSION_CODES.N)
 fun main() {
-
-    val g = { a: Int, b: Int,c:Int -> max( a + b + c, a * b * c) }
-    val h = g(2,2,3)
+    val g = { a: Int, b: Int, c: Int -> max(a + b + c, a * b * c) }
+    val h = g(2, 2, 3)
     println(h)
 }
